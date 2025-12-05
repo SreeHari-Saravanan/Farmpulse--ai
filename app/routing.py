b95e42df39bb9fbb756186680115dd42dbb6afa7
SREE HARI S <158766587+SreeHari-Saravanan@users.noreply.github.com>
2025-12-05 10:04:14 +0530

    Delete app directory

--- app/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path('ws://127.0.0.1:8000/ws/consultation/farmer_vet_room/', consumers.ConsultationConsumer.as_asgi()),
-]
